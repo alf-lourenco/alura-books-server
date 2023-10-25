27523b63447f247aaf52063ea620a3ed57a51e11
alfredo <gabriel94alfredo@gmail.com>
2023-10-25 18:22:25 -0300

    criando a primeira rota

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const express= require("express")
+const express = require('express')
 const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+   res.send("Óla Alfredooooo")
+})
+
+app.listen(port, () => {
+   console.log(`Escutando a porta ${port}`)
+})
